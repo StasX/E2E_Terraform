7b89d4bc77b9e9973e4715572bc276426b05ea42
Stanislav Mestechkin <s.mestechkin@gmail.com>
2026-04-27 18:35:00 +0000

    Add instance

---
+++ terraform/instance.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "builder" {
+  ami = "ami-098e39bafa7e7303d"
+  instance_type               = "t3.medium"
+  availability_zone           = var.region
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = module.infra.subnet_ids[0]
+  key_name                    = module.keygen.key_name
+  associate_public_ip_address = true
+  tags = {
+    "Name"    = "builder"
+    "Created" = "Terraform"
+  }
+  depends_on = [module.infra.subnet_ids, aws_security_group.sg, module.keygen.key_name]
+}
