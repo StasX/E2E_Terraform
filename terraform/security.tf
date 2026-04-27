7d91862cab2d9911a9a9c1463d0cc90bc4221d41
Stanislav Mestechkin <s.mestechkin@gmail.com>
2026-04-27 18:25:05 +0000

    Add Security Group

---
+++ terraform/security.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "sg" {
+  name        = "sg"
+  description = "Security group"
+  vpc_id      = module.infra.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 5001
+    to_port     = 5001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Name"    = "Security Group"
+    "Created" = "Terraform"
+  }
+}
