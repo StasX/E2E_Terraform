56a21ab33f217004973b02c7105af73e512e2706
Stanislav Mestechkin <s.mestechkin@gmail.com>
2026-04-27 19:06:24 +0000

    Fix output variables names

--- terraform/modules/keygen/outputs.tf
+++ terraform/modules/keygen/outputs.tf
@@ -1,11 +1,11 @@
 # Output the necessary details
-output "ssh_private_key_path" {
+output "key_path" {
   description = "Path to the generated private SSH key"
   sensitive   = true
   value       = local_file.private_key.filename
 }
 
-output "ssh_key_name" {
+output "key_name" {
   description = "Name of the AWS SSH key pair"
   value       = aws_key_pair.instance_key.key_name
 }
